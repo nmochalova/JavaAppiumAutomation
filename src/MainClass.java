b750d127fe555a6c8b9fe8730758165aa382c7df
NatalyM <nbelokopytova@gmail.com>
2021-11-09 16:52:27 +0300

    Homework Test_2

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,5 +2,11 @@ public class MainClass {
     public int getLocalNumber() {
         return 14;
     }
+
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
+    }
 }
 
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -4,7 +4,14 @@ import org.junit.Test;
 public class MainClassTest extends MainClass
 {
     @Test
-    public void testGetLocalNumber(){
+    public void testGetLocalNumber()
+    {
         Assert.assertTrue("Error: getLocalNumber != 14",this.getLocalNumber()==14);
+    }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("Error: getClassNumber < 45",this.getClassNumber()>45);
     }
 }
