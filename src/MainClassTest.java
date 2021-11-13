41ed7780b81150bad84ae5ad69fe2ec944bbe617
NatalyM <nbelokopytova@gmail.com>
2021-11-13 23:12:09 +0300

    Тест 3: исправлена ошибка теста (добавлен оператор ||)

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -3,29 +3,21 @@ import org.junit.Test;
 
 import java.util.Locale;
 
-public class MainClassTest extends MainClass
-{
+public class MainClassTest extends MainClass {
     @Test
-    public void testGetLocalNumber()
-    {
-        Assert.assertTrue("Error: getLocalNumber != 14",this.getLocalNumber()==14);
+    public void testGetLocalNumber() {
+        Assert.assertTrue("Error: getLocalNumber != 14", this.getLocalNumber() == 14);
     }
 
     @Test
-    public void testGetClassNumber()
-    {
-        Assert.assertTrue("Error: getClassNumber < 45",this.getClassNumber()>45);
+    public void testGetClassNumber() {
+        Assert.assertTrue("Error: getClassNumber < 45", this.getClassNumber() > 45);
     }
 
     @Test
-    public void testGetClassString()
-    {
-     String textString = this.getClassString().toUpperCase();
+    public void testGetClassString() {
+        Assert.assertTrue("Error: class_string does not contain 'Hello' or 'hello' ",
+        this.getClassString().contains("Hello") || this.getClassString().contains("hello"));
 
-     System.out.println(textString);
-     System.out.println( textString.contains("HELLO"));
-
-     Assert.assertTrue("Error: class_string does not contain 'Hello' or 'hello' ",
-             textString.contains("HELLO"));
     }
 }
