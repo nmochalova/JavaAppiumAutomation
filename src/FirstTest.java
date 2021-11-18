b450826d8a64a15c149ebb989411a80240f04f38
NatalyM <nbelokopytova@gmail.com>
2021-11-18 16:43:02 +0300

    Тест c очисткой поля ввода (testCancelSearch)

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -70,6 +70,19 @@ public class FirstTest {
                 5
         );
 
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5
+        );
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Cannot fined search field",
+                5
+        );
+
        waitForElementAndClick(
                By.id("org.wikipedia:id/search_close_btn"),
                 "Cannot find X to cancel search",
@@ -153,5 +166,12 @@ public class FirstTest {
         return wait.until(
                 ExpectedConditions.invisibilityOfElementLocated(by)
         );
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_messanger, long timeoutInSecond)
+    {
+        WebElement element = waitForElementPresent(by,error_messanger,timeoutInSecond);
+        element.clear();
+        return element;
     }
 }
